dd92d1ec39009ab6f9b02f0bada8f9da36599d8a
Harsh Patel <harsh.p3@ahduni.edu.in>
2021-11-07 14:19:34 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Main from './components/FileUpload';
+
+const App = () => (
+  <div>
+    <h1>File Upload</h1>
+    <Main />
+  </div>
+);
+
+export default App;
